470995d453ee0fcebc576c4b87be9e9969f7958e
Miguelhsa <miguelhsa@gmail.com>
2026-09-06 18:11:08 +0200

    Comunicador: envio real por Telegram

--- comunicador/notificador.py
+++ comunicador/notificador.py
@@ -1,5 +1,13 @@
+from dotenv import load_dotenv
+import os
+import httpx
 
+load_dotenv()
+TELEGRAM_TOKEN = os.environ["TELEGRAM_TOKEN"]
+TELEGRAM_CHAT_ID = os.environ["TELEGRAM_CHAT_ID"]
 
+url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
 def enviar(mensaje:str) -> None:
-    print(mensaje)
+
+    return httpx.post(url, data={"chat_id": TELEGRAM_CHAT_ID, "text": mensaje})
